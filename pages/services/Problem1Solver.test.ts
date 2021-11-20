c4d14af8cfe44fb14352eb803b253d7e50465c5f
David Veszelovszki <veszelovszki@gmail.com>
2021-11-20 17:02:55 +0100

    Implemented the solution for problem 1.a

--- pages/services/Problem1Solver.test.ts
+++ pages/services/Problem1Solver.test.ts
@@ -2,11 +2,24 @@ import Problem1Solver from './Problem1Solver';
 
 test('Problem1Solver can solve a trivial problem', async () => {
     /* Assemble */
-    const input=[1,2,3];
+    const input = [1, 2, 3];
 
     /* Act */
     const output = Problem1Solver.solve(input, 3);
 
     /* Assert */
     expect(output).toBe(2);
+});
+
+
+test('Problem1Solver can solve the sample problem', async () => {
+    /* Assemble */
+    const input = [1721, 979, 366, 299, 675, 1456];
+    const expectedResult = 514579;
+
+    /* Act */
+    const output = Problem1Solver.solve(input, 2020);
+
+    /* Assert */
+    expect(output).toBe(expectedResult);
 });
--- pages/services/Problem1Solver.ts
+++ pages/services/Problem1Solver.ts
@@ -1,5 +1,24 @@
 export default class Problem1Solver {
-    static solve(numbers: number[], targetSum: number): number {
-        return numbers[0] * numbers[1]; /* TODO: Develop this */
+    /* Got inspiration for an O(n) solution from LeetCode: https://leetcode.com/problems/two-sum/ */
+    static solve(numbers: number[], target: number): number {
+        /* Create map to search in O(1) time */
+        const numberMap = new Map<number, number>();
+
+        /* Loop through each number */
+        for (let i = 0; i < numbers.length; i++) {
+            // Find the difference between the target and number.
+            const difference = target - numbers[i]
+
+            /* Check numberMap for the difference. If it's already in the map, calculate and return the result. */
+            if (numberMap.has(difference)) {
+                return numbers[numberMap.get(difference) || 0] * numbers[i];
+            }
+
+            /* Store the number and its index in the map. */
+            numberMap.set(numbers[i], i)
+        }
+
+        /* Return -1 if it found no solution. */
+        return -1;
     }
 }
